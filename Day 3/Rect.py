966ba120be11b9a9a730f302bf51d1f9cf3f25fd
Disha Kamra <dishakamra@gmail.com>
2025-10-08 13:42:11 -0400

    day3

--- Day 3/Rect.py
+++ Day 3/Rect.py
@@ -1,1 +1,11 @@
 # Write a Rectangle with width and height. Add an instance method area() that returns width * height
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+r = Rectangle(3, 4)
+print(r.area()) # 12
