4df0c4081d954e7bc0e3ab9d6e87ed8ecda09f66
Disha Kamra <dishakamra@gmail.com>
2025-10-09 16:58:32 -0400

    day4

--- Day 4/test_currency.py
+++ Day 4/test_currency.py
@@ -1,1 +1,16 @@
 # Test it with monkeypatch
+
+# test_currency.py
+import currency
+
+class MockResponse:
+    def json(self):
+        return {"rate": 1.40}
+
+def test_get_usd_to_inr(monkeypatch):
+    def mock_get(url):
+        return MockResponse()
+
+    monkeypatch.setattr(currency.requests, "get", mock_get)
+
+    assert currency.get_usd_to_cad() == 1.40
